413208708fc303677213a8c669de2a5933e0b41d
jakiyonep <akiyone0820@gmail.com>
2020-08-02 12:46:18 +0900

    a

--- nihongosensei/settings/base.py
+++ nihongosensei/settings/base.py
@@ -179,4 +179,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'atsumarenihongokyoushi@gmail.com'
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_PASSWORD = os.environ.get('qnnqhxqnnzdtbdov')
+EMAIL_HOST_PASSWORD = 'qnnqhxqnnzdtbdov'
