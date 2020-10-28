39b8a38316a12725bd4ade3568a706e256f290d8
unknown <akt.handbal0820@gmail.com>
2020-10-28 18:26:48 +0900

    a

--- nihongosensei/settings/__init__.py
+++ nihongosensei/settings/__init__.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 from .production import *
 
 try:
     from .local import *
 except:
-=======
-from .production import *
-
-try:
-    from .local import *
-except:
->>>>>>> 287a091a6babc61a039735628df9dc39e3fe3a9b
     pass
